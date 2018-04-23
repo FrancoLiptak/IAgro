2df644d06d4df2f5de36a4323ba8971d92378e15
Franco Liptak <francoliptak@gmail.com>
2018-04-23 20:28:47 -0300

    Saca fotos, guarda con error

---
+++ app/src/main/java/com/fg/franco/i_agro/MainActivity.java
@@ -0,0 +1,73 @@
+package com.fg.franco.i_agro;
+
+import android.hardware.Camera;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    Camera camera;
+    FrameLayout frameLayout;
+    ShowCamera showCamera;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
+        camera = Camera.open();
+        showCamera = new ShowCamera(this, this.camera);
+        frameLayout.addView(this.showCamera);
+
+    }
+
+    Camera.PictureCallback mPicktureCallback = new Camera.PictureCallback(){
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera){
+            File picture_file = getOutputMediaFile();
+            if (picture_file == null){
+                return;
+            }else{
+                try{
+                    FileOutputStream fos = new FileOutputStream(picture_file);
+                    fos.write(data);
+                    fos.close();
+                    camera.stopPreview();
+                }catch(IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    private File getOutputMediaFile(){
+        String state = Environment.getExternalStorageState();
+        if(!state.equals((Environment.MEDIA_MOUNTED))){
+            return null;
+        }else{
+            File folder_gui = new File(Environment.getExternalStorageDirectory() + File.separator + "GUI");
+            if(!folder_gui.exists()){
+                folder_gui.mkdirs();
+            }
+
+            File outputFile = new File(folder_gui,"temp.jpg");
+            return outputFile;
+        }
+
+    }
+
+    public void captureImage(View v){
+        if(this.camera != null){
+            this.camera.takePicture(null, null, mPicktureCallback);
+        }
+    }
+}
---
+++ app/src/main/java/com/fg/franco/i_agro/ShowCamera.java
@@ -0,0 +1,56 @@
+package com.fg.franco.i_agro;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.util.List;
+
+public class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
+    Camera camera;
+    SurfaceHolder holder;
+
+    public ShowCamera(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        this.holder = getHolder();
+        this.holder.addCallback(this);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Camera.Parameters parameters = this.camera.getParameters();
+
+        if(this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE){
+            parameters.set("orientation", "portrait");
+            this.camera.setDisplayOrientation(90);
+            parameters.setRotation(90);
+        }else{
+            parameters.set("orientation", "landscape");
+            this.camera.setDisplayOrientation(0);
+            parameters.setRotation(0);
+        }
+        this.camera.setParameters(parameters);
+
+        try{
+            this.camera.setPreviewDisplay(this.holder);
+            this.camera.startPreview();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        this.camera.stopPreview();
+        this.camera.release();
+    }
+}
