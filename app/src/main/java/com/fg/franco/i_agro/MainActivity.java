f920ab7d1c24cfda8e1d9d3a8b5810e6c247d857
Franco Liptak <francoliptak@gmail.com>
2018-06-03 01:13:03 -0300

    Fix

--- app/src/main/java/com/fg/franco/i_agro/MainActivity.java
+++ app/src/main/java/com/fg/franco/i_agro/MainActivity.java
@@ -43,102 +43,72 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
 
     Camera camera;
     FrameLayout frameLayout;
-    ShowCamera showCamera;
-    RandomAnalyzer analyzer = new RandomAnalyzer();
     ResultDialog resultDialog;
     ImageView image;
+    Button buttonGallery;
+    RandomAnalyzer analyzer = new RandomAnalyzer();
+    ShowCamera showCamera;
     static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
     static final int MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE = 1;
     static final int PICK_IMAGE_REQUEST= 2;
     static final int REQUEST_PERMISSION_SETTING = 3;
-    Button buttonGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
         if (this.checkCameraPermissions() && this.checkStoragePermissions()){
             camera = this.getCameraInstance();
             showCamera = new ShowCamera(this, this.camera);
             frameLayout.addView(this.showCamera);
         }
-
         image = (ImageView)findViewById(R.id.imageView);
         resultDialog = new ResultDialog();
         resultDialog.setAnalyzer(analyzer);
-
     }
 
-    /** A safe way to get an instance of the Camera object. */
     public static Camera getCameraInstance(){
         Camera c = null;
         try {
             c = Camera.open(); // attempt to get a Camera instance
         }
         catch (Exception e){
-            // Camera is not available (in use or does not exist)
+            Log.d("I-Agro", "Camera is not available");
         }
         return c; // returns null if camera is unavailable
     }
 
 
     private boolean checkCameraPermissions() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.CAMERA},
-                    MY_PERMISSIONS_REQUEST_CAMERA);
-
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
             return false;
-
-        }else return true;
+        }else{
+            return true;
+        }
     }
     private boolean checkStoragePermissions() {
-        if ((ContextCompat.checkSelfPermission(this,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-            || (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)){
-
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},
-                    MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE);
-
+        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) || (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE);
             return false;
-
-        }else return true;
+        }else{
+            return true;
+        }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_CAMERA: {
                 // If request is cancelled, the result arrays are empty.
                 if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
                     boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA);
-                    if (! showRationale) {
-                        // user also CHECKED "never ask again"
-                        // you can either enable some fall back,
-                        // disable features of your app
-                        // or open another dialog explaining
-                        // again the permission and directing to
-                        // the app setting
-
-                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    if (! showRationale) { // user also checked "never ask again"
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS); // let's go to the configuration of the application
                         Uri uri = Uri.fromParts("package", getPackageName(), null);
                         intent.setData(uri);
                         startActivityForResult(intent, REQUEST_PERMISSION_SETTING);
-
-                    } else {
-                        // user did NOT check "never ask again"
-                        // this is a good place to explain the user
-                        // why you need the permission and ask if he wants
-                        // to accept it (the rationale)
                     }
                 }
                 return;
@@ -146,82 +116,55 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
             case MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE: {
                 // If request is cancelled, the result arrays are empty.
                 if (!(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
                     boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-                    if (! showRationale) {
-                        // user also CHECKED "never ask again"
-                        // you can either enable some fall back,
-                        // disable features of your app
-                        // or open another dialog explaining
-                        // again the permission and directing to
-                        // the app setting
-
-                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    if (! showRationale) { // user also CHECKED "never ask again"
+                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS); // let's go to the configuration of the application
                         Uri uri = Uri.fromParts("package", getPackageName(), null);
                         intent.setData(uri);
                         startActivityForResult(intent, REQUEST_PERMISSION_SETTING);
-
-                    } else {
-                        // user did NOT check "never ask again"
-                        // this is a good place to explain the user
-                        // why you need the permission and ask if he wants
-                        // to accept it (the rationale)
                     }
                 }
                 return;
             }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
     }
 
     Camera.PictureCallback mPictureCallback = new Camera.PictureCallback(){
         @Override
         public void onPictureTaken(byte[] data, Camera camera){
-            File picture_file = getOutputMediaFile();
-            if (picture_file == null){
-                return;
-            }
-            try{
-                FileOutputStream fos = new FileOutputStream(picture_file);
-                fos.write(data);
-                fos.close();
-
-                resultDialog.show(getFragmentManager(), "result");
-
-            }catch(IOException e){
-                e.printStackTrace();
-            }
+        File picture_file = getOutputMediaFile();
+        if (picture_file == null){
+            return;
+        }
+        try{
+            FileOutputStream fos = new FileOutputStream(picture_file);
+            fos.write(data);
+            fos.close();
+            resultDialog.show(getFragmentManager(), "result");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
         }
     };
 
     private File getOutputMediaFile(){
         String state = Environment.getExternalStorageState();
-
-
         if(!state.equals((Environment.MEDIA_MOUNTED))){
             return null;
         }else{
-            File folder_gui = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-                    "I-Agro");
+            File folder_gui = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),"I-Agro");
             if(!folder_gui.exists()){
                 if (!folder_gui.mkdirs()) {
                     Log.d("I-Agro", "failed to create directory");
                     return null;
                 }
             }
-
             String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
             String imageFileName = "IMG_" + timeStamp + ".jpg";
             File outputFile = new File(folder_gui.getPath() + File.separator + imageFileName);
             MediaScannerConnection.scanFile(this, new String[] { outputFile.getPath() }, new String[] { "image/jpeg" }, null);
             return outputFile;
         }
-
     }
 
     public void captureImage(View v){
@@ -238,10 +181,8 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
 
     private void uploadImage() {
         Intent intent = new Intent();
-        // Show only images, no videos or anything else
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-        // Always show the chooser (if there are multiple options available)
+        intent.setType("image/*"); // Show only images
+        intent.setAction(Intent.ACTION_GET_CONTENT); // Always show the chooser (if there are multiple options available)
         startActivityForResult(Intent.createChooser(intent, "Seleccione una imagen"), PICK_IMAGE_REQUEST);
     }
 
@@ -252,14 +193,11 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
             Uri uri = data.getData();
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                // Log.d(TAG, String.valueOf(bitmap));
-
                 image.setVisibility(View.VISIBLE);
                 image.setImageBitmap(bitmap);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
             resultDialog.show(getFragmentManager(), "result");
         }
     }
@@ -291,10 +229,9 @@ public class MainActivity extends AppCompatActivity implements DialogInterface.O
 
     private void releaseCamera(){
         if (camera != null){
-            camera.release();        // release the camera for other applications
-            camera = null;
+            camera.release(); // release the camera for other applications
             frameLayout.removeView(this.showCamera);
-            showCamera = null;
+            camera = null; showCamera = null; // importante
         }
     }
 
